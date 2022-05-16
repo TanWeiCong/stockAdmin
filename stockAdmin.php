f18ee08049a8a245eb54741df7e32414a36f707e
tanweijian828 <tweijian28@gmail.com>
2022-05-16 23:30:53 +0800

    stockAdmin

---
+++ stockAdmin.php
@@ -0,0 +1,97 @@
+<?php
+include "header_admin.php"
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" href="css/styleStockAdmin.css">
+</head>
+
+<body>
+  <div class="row">
+    <div class="right">
+      <div class="button-container">
+        <button class="button button2">Add Book</button>
+      </div>
+      <!-- <div class="button-container">
+        <button class="button button2">Update</button>
+      </div> -->
+      
+    </div>
+  </div>
+
+
+  <div class="row">
+    <div class="center">
+      <div class="column">
+      <a href="update_book.php">
+        <div class="polaroid">
+          <img src="images/bc.jpg" alt="5 Terre" height=300 width=210>
+          <div class="container">
+            <p class="book-information">Book Title:</br></br>
+              ISBN-13 number</br></br>
+              Quantity:</p>
+          </div>
+        </div>
+      </a>
+      </div>
+
+      <div class="column">
+      <a href="update_book.php">
+        <div class="polaroid">
+          <img src="images/bc.jpg" alt="5 Terre" height=300 width=210>
+          <div class="container">
+            <p class="book-information">Book Title:</br></br>
+              ISBN-13 number</br></br>
+              Quantity:</p>
+          </div>
+        </div>
+      </a>
+      </div>
+
+      <div class="column">
+      <a href="update_book.php">
+        <div class="polaroid">
+          <img src="images/bc.jpg" alt="5 Terre" height=300 width=210>
+          <div class="container">
+            <p class="book-information">Book Title:</br></br>
+              ISBN-13 number</br></br>
+              Quantity:</p>
+          </div>
+        </div>
+      </a>
+      </div>
+
+      <div class="column">
+      <a href="update_book.php">
+        <div class="polaroid">
+          <img src="images/bc.jpg" alt="5 Terre" height=300 width=210>
+          <div class="container">
+            <p class="book-information">Book Title:</br></br>
+              ISBN-13 number</br></br>
+              Quantity:</p>
+          </div>
+        </div>
+      </a>
+      </div>
+
+      <div class="column">
+      <a href="update_book.php">
+        <div class="polaroid">
+          <img src="images/bc.jpg" alt="5 Terre" height=300 width=210>
+          <div class="container">
+            <p class="book-information">Book Title:</br></br>
+              ISBN-13 number</br></br>
+              Quantity:</p>
+          </div>
+        </div>
+      </a>
+      </div>
+    </div>
+  </div>
+
+</body>
+
+</html>
